6f3793f72a3f99ab5d5d8d8a2f5fa0482fe5e6e7
POSRIVAS <POSRIVAS@POSRIVAS-PORTEG.us.oracle.com>
2021-12-19 13:01:04 +0530

    dummy

---
+++ src/main/java/com/healthykhao/server/HealthykhaoApplication.java
@@ -0,0 +1,13 @@
+package com.healthykhao.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthykhaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthykhaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthykhao/server/HealthykhaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthykhao.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthykhaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
